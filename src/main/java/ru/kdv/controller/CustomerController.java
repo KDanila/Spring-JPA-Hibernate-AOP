0b3f71aede656b140c77916980b799b5b717e90a
KDanila <concista@gmail.com>
2020-02-04 11:23:47 +0300

    Refactor.

--- src/main/java/ru/kdv/controller/CustomerController.java
+++ src/main/java/ru/kdv/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CustomerController {
 	@GetMapping("/list")
 	public String listCustomers(Model model) {
+
 		return "list-customers";
 	}
 }
